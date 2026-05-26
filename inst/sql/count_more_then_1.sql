9835cb3663a0178d4e5410cace5cd40103d8adbc
Mattia Cinelli <cinelli.mattia@gmail.com>
2026-05-26 17:14:31 +0200

    Update inst/sql/count_more_then_1.sql
    Co-authored-by: Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>

--- inst/sql/count_more_then_1.sql
+++ inst/sql/count_more_then_1.sql
@@ -12,7 +12,7 @@ INNER JOIN concepts AS c
     ON c.person_id = w.person_id
    AND c.concept_id = w.concept_id
    AND c.date BETWEEN w.window_start AND w.window_end
-WHERE w.selector = 'COUNT_MORE_THEN_1'
+WHERE w.selector = 'COUNT_MORE_THAN_1'
 GROUP BY
     w.anchor_row_id,
     w.person_id,
