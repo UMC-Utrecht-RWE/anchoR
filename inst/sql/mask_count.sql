4bba1d17ffc5004787ae4c4599a70b307fdc8b1f
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2026-07-28 11:41:57 +0200

    renamed value into count becauese I need to do the count first

--- inst/sql/mask_count.sql
+++ inst/sql/mask_count.sql
@@ -5,11 +5,10 @@ SELECT
     w.window_name,
     CAST(
         CASE
-            WHEN w.value = 0 THEN 1       -- Value of 0 -> assign 1
-            WHEN w.value IN (1, 2) THEN 2 -- Value of 1 or 2 -> assign 2
-            WHEN w.value IN (3, 4) THEN 3 -- Value of 3 or 4 -> assign 3
-            WHEN w.value >= 5 THEN 4      -- Value of ≥ 5 -> assign 4
-        ELSE -1 -- Unmatched
+            WHEN COUNT(*) = 0 THEN 1      -- count of 0 -> assign 1
+            WHEN COUNT(*) IN (1, 2) THEN 2 -- count of 1 or 2 -> assign 2
+            WHEN COUNT(*) IN (3, 4) THEN 3 -- count of 3 or 4 -> assign 3
+            WHEN COUNT(*) >= 5 THEN 4     -- count of >= 5 -> assign 4
         END AS VARCHAR
     ) AS value,
     MAX(c.date) AS date,
@@ -19,7 +18,7 @@ INNER JOIN concepts AS c
     ON c.person_id = w.person_id
    AND c.concept_id = w.concept_id
    AND c.date BETWEEN w.window_start AND w.window_end
-WHERE w.selector = 'MASK_VALUES'
+WHERE w.selector = 'MASK_COUNT'
 GROUP BY
     w.person_id,
     w.T0,
